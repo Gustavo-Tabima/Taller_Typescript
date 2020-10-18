352e06cbc68022077e1e005ca0f592c7337a9947
Gustavo Tabima <60206565+Gustavo-Tabima@users.noreply.github.com>
2020-10-18 00:08:37 -0500

    Done
    Donete Donete

---
+++ scripts/dataCourses.js
@@ -0,0 +1,10 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Ingeniería de Sw", "Cesar Forero", 3),
+    new Course("Sistrans", "German Bravo", 3),
+    new Course("Diseño y Algoritmos", "Rodrigo", 3),
+    new Course("TI para las organizaciones", "Paola", 3),
+    new Course("Cálculo Integral", "CortiDios", 3),
+    new Course("Biología Celular", "Ana María", 3)
+    
+];
